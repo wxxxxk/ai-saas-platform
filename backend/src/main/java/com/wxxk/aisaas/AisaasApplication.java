0cdd7b418af24b01281e28b491b95f9945b19ec1
wxxxxk <leejaewook17@naver.com>
2026-04-02 15:51:05 +0900

    chore: initialize runnable frontend and backend

---
+++ backend/src/main/java/com/wxxk/aisaas/AisaasApplication.java
@@ -0,0 +1,13 @@
+package com.wxxk.aisaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisaasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wxxk/aisaas/AisaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxxk.aisaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
